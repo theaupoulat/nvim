bb9edca295a44480b7591ec46086eebf7a0cd48f
Theau Poulat <theau.poulat@gmail.com>
2025-03-27 13:26:47 +0100

    cleanup: remove leap.nvim

--- init.lua
+++ init.lua
@@ -235,13 +235,6 @@ require('lazy').setup({
   },
   'tpope/vim-repeat',
   {
-    'ggandor/leap.nvim', -- quick search to help on file navigation
-    config = function()
-      require('leap').create_default_mappings()
-    end,
-  },
-
-  {
     'dmmulroy/tsc.nvim', -- run and explore tsc errors
     config = function()
       require('tsc').setup()
