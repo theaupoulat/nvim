261afa5aefcdd4d31e74d233b07c2f504a90b6ca
Theau Poulat <theau.poulat@gmail.com>
2025-04-10 09:24:18 +0200

    feat: add tsc keymap

--- init.lua
+++ init.lua
@@ -240,6 +240,7 @@ require('lazy').setup({
       require('tsc').setup()
       vim.keymap.set('n', '<leader>to', ':TSCOpen<CR>', { desc = '[O]pen TSC window' })
       vim.keymap.set('n', '<leader>tc', ':TSCClose<CR>', { desc = '[C]lose TSC window' })
+      vim.keymap.set('n', '<leader>tt', ':TSC<CR>', { desc = '[T]SC' })
     end,
   },
   {
