e4c2e8dfd68612958059a531031bb981f1ed71f2
Theau Poulat <theau.poulat@gmail.com>
2025-04-28 19:39:01 +0200

    feat: add command for files absolute path

--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -2,5 +2,11 @@ vim.api.nvim_create_user_command("ShowFileType", function()
   print(vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(0), 'filetype'))
 end, {})
 
+vim.api.nvim_create_user_command("ShowFileName", function()
+  local file_name = vim.fn.expand('%:p')
+  vim.fn.setreg('+', file_name)
+  print(file_name)
+end, {})
+
 
 return {}
