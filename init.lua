a37b82da127e74bcf465a7b1595bfff528e4a0ef
Theau Poulat <theau.poulat@gmail.com>
2025-06-01 21:18:18 +0200

    fix: correct diagnostics QF shortcut and move closer

--- init.lua
+++ init.lua
@@ -103,9 +103,6 @@ vim.opt.scrolloff = 10
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = '[E]xtend [d]iagnostic text' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -126,6 +123,10 @@ vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>', { desc = '[Q]uickfix: next entry'
 vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>', { desc = '[Q]uickfix: previous entry' })
 vim.keymap.set('n', '<leader>qo', '<cmd>copen<CR>', { desc = '[Q]uickfix: open' })
 vim.keymap.set('n', '<leader>qc', '<cmd>cclose<CR>', { desc = '[Q]uickfix: close' })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>qd', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = '[E]xtend [d]iagnostic text' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
