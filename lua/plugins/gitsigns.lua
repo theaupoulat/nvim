87581690688b5ea5a4ad560db5b06f8af8ebc7be
Theau Poulat <theau.poulat@gmail.com>
2025-09-25 22:15:42 +0200

    feat: add vim-fugitive
    so i never have to leave this place

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<M-h>', ':Gitsigns prev_hunk<CR>', { desc = 'Previous hunk'
 vim.keymap.set('n', '<leader>hq', ':Gitsigns setqflist<CR>', { desc = 'git show hunks in [Q]uicklist' })
 
 return {
+  'tpope/vim-fugitive',
   {
     'lewis6991/gitsigns.nvim', -- Adds git related signs to the gutter, as well as utilities for managing changes
     opts = {
