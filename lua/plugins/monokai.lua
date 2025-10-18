c01a2f2fee33a53ddbdf16be8264bd834cebfa69
Theau Poulat <theau.poulat@gmail.com>
2025-10-18 10:27:26 +0200

    refactor: theme setup

--- lua/plugins/monokai.lua
+++ lua/plugins/monokai.lua
@@ -3,7 +3,7 @@ return {
   'loctvl842/monokai-pro.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    require('monokai-pro').setup { filter = 'spectrum' }
-    vim.cmd.colorscheme 'monokai-pro'
+    require('monokai-pro').setup {}
+    vim.cmd.colorscheme 'monokai-pro-spectrum'
   end,
 }
