31f04ca1f3c0a112007e244b066020e917419f3f
Theau Poulat <theau.poulat@gmail.com>
2025-10-23 10:35:54 +0200

    feat: add session manager and big head dashboard

---
+++ lua/plugins/session.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'olimorris/persisted.nvim',
+    event = 'BufReadPre', -- Ensure the plugin loads only when a buffer has been loaded
+    opts = {
+      -- Your config goes here ...
+    },
+  },
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,11 +8,70 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      preset = {
+        header = [[
+  .,;::clllcc:,'.
+  'oOK000K0OOOOOOOOOOOxo;
+    'x000OOOOOOOOOOOOOOOOOOOxl.
+   c00OOOkkkkkkkkkkOOOOOOOOkool.
+        .kxddooolllllloooooddxxxdoooc;,.
+        col:;;,'''''''',,;;::clooooc;;;;;.
+        ;,'''''';dkkoc,''''''''',;;;;;;;;;.
+        .''''''lKKKOOko,'''''''''';;;;;;;;;
+         ;clooxKK0OOkoo:;cloddoc;',;;;;;;;;
+         xkOkd0KOOOxoc;:odkO00OOOd;;;;;;;;;.
+       .OKOOkK0OOOdo:;;ookO0OOOOOl;;;;;;;;;.
+      ,OOOO0K0OOkdc;;;;coxOOOOOOk;;;;;;;;;;.
+      OOO0KKOOOkoooddoc;coxOOOOOo;;;;;;;;;;.
+   oOO000OOkxooxOOkxdl;;odkOOO:;;;;;;;;;;.
+   OOxolcc::::cloooooo:;coodkx;;;;;;;;;;;.
+   OOl;',;;,''',;:ccc:;;looooc;;;;;;;;;;;.
+  .OOOOxoolllc:;::::::cloooooc;;;;;;;;;;;'
+  'OOO00OOOOOOOkdoooooooooooo:;;;;;;;;;;;;
+  ;Oxocc:::::cclodooooooooooo:;;;;;;;;;;;.
+  ;kcccccccccccclcc:loooooooo:;;;;;;;;;;
+  ;Okdlc:;;;;:clodxxxooooooooc;;;;;;.
+  ;OOOxoooooooooxOOOOkoooooooc;;;;;;,
+  :OOOOkdooooodkOOOOOOoooooooc;;;;;;;
+  kOOOOOOOkkkOOOOOOOOOxoooooo:;;;;;;;
+  OOOOOO00OOOOOOOOOOOOkoooooo:;;;;;;;
+ .dkkOOO00OOOOOOOOOOOkxoooool;;;;;;;.
+   .ooddddxxxddddddooooooolc;;;,
+					]],
+      },
+
+      sections = {
+        { section = 'header' },
+
+        { section = 'keys', gap = 1, padding = 1 },
+        { section = 'startup', icon = '' },
+        function()
+          local cwd = vim.fn.getcwd()
+          local parent_dir, project_name = string.match(cwd, '(.-)/([^/]+)$')
+
+          if parent_dir == nil then
+            -- Handle the case where there's no slash (e.g., if cwd is just "/")
+            parent_dir = ''
+            project_name = cwd
+          end
+
+          return {
+            align = 'center',
+            text = {
+              { 'at ' .. parent_dir .. '/', hl = 'footer' },
+              { project_name, hl = 'special' },
+              -- { cwd,                        hl = "special" },
+            },
+          }
+        end,
+      },
+    },
     explorer = { enabled = false },
     indent = { enabled = true },
     input = { enabled = true },
-    git;rowse = { enabled = true },
+    git,
+    rowse = { enabled = true },
     picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
@@ -61,7 +120,7 @@ return {
     {
       '<leader>gB',
       function()
-        Snacks.gitbrowse({branch = 'main'})
+        Snacks.gitbrowse { branch = 'main' }
       end,
       desc = 'Git Browse',
       mode = { 'n', 'v' },
