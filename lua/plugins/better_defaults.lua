6272a43b86c328ec483dd039da0ad3698e1a35c7
Theau Poulat <theau.poulat@gmail.com>
2025-10-30 13:43:47 +0100

    cleanup: group plugins that enhance neovim default capabilities

---
+++ lua/plugins/better_defaults.lua
@@ -0,0 +1,47 @@
+return {
+  'kevinhwang91/nvim-bqf',
+  {
+    'chentoast/marks.nvim',
+    event = 'VeryLazy',
+    opts = {
+      default_mappings = true,
+    },
+  },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    config = function()
+      vim.o.foldcolumn = '1' -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      require('ufo').setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
+      }
+    end,
+  },
+  {
+    'max397574/better-escape.nvim', -- escapes on jj
+    config = function()
+      require('better_escape').setup()
+    end,
+  },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'monokai-pro',
+        },
+      }
+    end,
+  },
+}
--- lua/plugins/better_escape.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'max397574/better-escape.nvim', -- escapes on jj
-  config = function()
-    require('better_escape').setup()
-  end,
-}
