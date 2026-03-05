b406b7bac61a402b7c91630304f6df3643cc9c3b
Theau Poulat <theau.poulat@gmail.com>
2026-03-05 21:43:30 +0100

    chore: close multi line comments

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@
 ========                                                     ========
 =====================================================================
 =====================================================================
-
+-- ]]
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
